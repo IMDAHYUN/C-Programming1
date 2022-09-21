e3806800015eb10093100959ec836127f0bbdb99
IMDAHYUN <113669826+IMDAHYUN@users.noreply.github.com>
2022-09-21 16:58:19 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	double km, miles;
+	printf("Please enter kilometers : ");
+	scanf("%lf", &km);
+
+	miles = km / 1.609;
+	printf("%.1f km is equal to %.1f miles.", km, miles);
+}
